80ba33e0d5df6090004a919d54a9efaa17bf64a0
camyhsu <camyhsu21@gmail.com>
2019-11-24 19:07:05 -0800

    Complete name data loading

--- tools/year_book/add_name_to_file/lib/add_name_to_file/name_data_loader.ex
+++ tools/year_book/add_name_to_file/lib/add_name_to_file/name_data_loader.ex
@@ -1,7 +1,18 @@
 defmodule AddNameToFile.NameDataLoader do
+  @student_id_key "Student ID"
+  @english_first_name_key "First Name"
+  @english_last_name_key "Last Name"
+  @chinese_name_key "Chinese Name"
+
   def load(data_file_path) do
     File.stream!(data_file_path, encoding: :utf8)
     |> CSV.decode!(headers: true)
-    |> Enum.to_list()
+    |> Map.new(fn x -> {x[@student_id_key], record_to_name_string(x)} end)
+  end
+
+  def record_to_name_string(name_data_map) do
+    "#{name_data_map[@student_id_key]}_#{name_data_map[@english_first_name_key]}_#{
+      name_data_map[@english_last_name_key]
+    }_#{name_data_map[@chinese_name_key]}"
   end
 end
--- tools/year_book/add_name_to_file/test/name_data_loader_test.exs
+++ tools/year_book/add_name_to_file/test/name_data_loader_test.exs
@@ -1,12 +1,45 @@
 defmodule NameDataLoaderTest do
   use ExUnit.Case
 
-  import AddNameToFile.NameDataLoader, only: [load: 1]
+  import AddNameToFile.NameDataLoader, only: [load: 1, record_to_name_string: 1]
 
   @test_name_data_file "test/resources/test_name_data.csv"
 
   test "load should load the csv data file into a map of student id to name" do
     result = load(@test_name_data_file)
-    IO.inspect(result)
+
+    assert result == %{
+             "6118" => "6118_Bennett_Lee_李景軒",
+             "6698" => "6698_Victoria_Chiu_邱凱蒂",
+             "6703" => "6703_Sebastien_Ho_何敏求",
+             "6787" => "6787_Joy_Huang_黄静然",
+             "7006" => "7006_Remy_Lai_賴君睿",
+             "7128" => "7128_Celina_Lin_",
+             "7166" => "7166_Ryan_Ho_何彦君",
+             "7240" => "7240_Rachel_Mandel_陸敏慧",
+             "7510" => "7510_Isabella_Lee_"
+           }
+  end
+
+  test "record_to_name_string should construct the name string based on the record given" do
+    assert "6118_Bennett_Lee_李景軒" ==
+             record_to_name_string(%{
+               "Chinese Name" => "李景軒",
+               "Class" => "1A",
+               "First Name" => "Bennett",
+               "Gender" => "M",
+               "Last Name" => "Lee",
+               "Student ID" => "6118"
+             })
+
+    assert "7128_Celina_Lin_" ==
+             record_to_name_string(%{
+               "Chinese Name" => "",
+               "Class" => "1A",
+               "First Name" => "Celina",
+               "Gender" => "F",
+               "Last Name" => "Lin",
+               "Student ID" => "7128"
+             })
   end
 end
