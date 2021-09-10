5e85f56c08967fcd726032ec4e104e665bea719d
Eugene Chang <6447186+EugeneChang@users.noreply.github.com>
2021-09-09 18:14:28 -0700

    Add Registration Payment

---
+++ tocs/back-end/src/utils/email.js
@@ -0,0 +1,31 @@
+import config from 'config';
+
+const contacts = config.get('contacts');
+
+const sendPaymentConfirmation = ({ recipient }) => {
+  const obj = {
+    subject: 'Thousand Oaks Chinese School - Payment Confirmation',
+    recipient,
+  };
+  console.log(obj);
+};
+
+const sendTextBookNotification = () => {
+  const obj = {
+    subject: 'TOCS - text books for new students',
+    recipient: contacts.textBookManager,
+  };
+  console.log(obj);
+};
+
+const sendRegistrationStaffNotification = () => {
+  const obj = {
+    subject: 'TOCS - new students registered',
+    recipient: contacts.registration,
+  };
+  console.log(obj);
+};
+
+export {
+  sendPaymentConfirmation, sendTextBookNotification, sendRegistrationStaffNotification,
+};
