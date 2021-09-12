f8834e3a9ef68ede1eb3830620dac30256ef0378
Eugene Chang <6447186+EugeneChang@users.noreply.github.com>
2021-09-11 17:34:39 -0700

    add access token check

--- tocs/front-end/src/services/data.service.js
+++ tocs/front-end/src/services/data.service.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 const API_URL = 'http://localhost:3001/api/';
 const headers = { headers: authHeader() };
 
-const get = async (path) => axios.get(API_URL + path, headers);
+const get = async (path) => headers.headers['x-access-token'] ? axios.get(API_URL + path, headers) : Promise.reject('No access token');
 const post = async (path, obj) => axios.post(API_URL + path, obj, headers);
 const put = async (path, obj) => axios.put(API_URL + path, obj, headers);
 const _delete = async (path) => axios.delete(API_URL + path, headers);
