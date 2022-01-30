c10c0a0170c877643301172da570d9ef4e82c2cb
Chiaming Hsu <camyhsu21@gmail.com>
2022-01-30 09:08:26 -0800

    front-end -- fix the only one test so far

--- tocs/front-end/src/App.test.js
+++ tocs/front-end/src/App.test.js
@@ -1,8 +1,17 @@
 import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import store from "./store";
 import App from "./App";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("The TOCS front-end application", () => {
+  test("should show the Login screen by default", () => {
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+
+    expect(screen.getByText("Username")).toBeInTheDocument();
+    expect(screen.getByText("Password")).toBeInTheDocument();
+  });
 });
